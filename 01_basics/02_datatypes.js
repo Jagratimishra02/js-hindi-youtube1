74d554d7299abcac8c5513c87c091c73e85137e8
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-02 05:53:27 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" //treat all js code as a newer version
+
+//  alert("2+5") // we r using node js not browser
+
+console.log(3+3);
+console.log("jagrati");
+
+let name = "Jagrati"
+let age =21
+let isloggenIn =false 
+
+//  number=> 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false 
+// null => standalone value
+// undefined => value is not assigned
+// symbol => uniqueness
+// object
+ 
+console.log(typeof age); // age is number 
+console.log(typeof null); //type of null is object 
+console.log(typeof  undefined); // type of undefined is undefined 
+
+
+
