7309b3d464a70e5d8651f120c8f4c84599ca3313
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-02 04:45:40 +0000

    new

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 144553
+let accountEmail = "jagrati@gmail.com"
+var accountpassword ="12345"
+accountcity = "Jaipur"
+//lets change
+// accountId = 2 // not allowed
+accountEmail = "jag@gmail.in"
+accountpassword = "1212121"
+accountcity = "pune"
+console.log(accountId);
+console.table ([accountId,accountpassword, accountcity])
