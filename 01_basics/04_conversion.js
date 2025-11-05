e7a799199e3abf4a3fbcd024ff8789656c6d1180
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-05 04:23:39 +0000

    conversions

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,30 @@
+// console.log(2>1);
+// true
+// console.log(2>=1);
+// true
+// console.log(2<=1);
+// false
+// console.log(2==1);
+// false
+// console.log(2!=1);
+// true
+
+
+// basic conversions (dont do such conversions if u want to write clean code of different data type comparisons)
+
+// console.log("2">1); // true  bcoz "2" is converted into number
+// console.log("2">"1");//true
+
+console.log(null>0); // false
+console.log(null ==0);//false
+console.log(null>=0); // true here null is converted into Number 0
+console.log( undefined = 0);
+console.log( undefined <= 0); // all the values in defined will be false 
+console.log( undefined => 0);
+
+
+
+
+
+// === strict check
+console.log("2" === 2); // false bcoz of strict check here the values r not converted into same datatypes
