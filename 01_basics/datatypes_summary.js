eba1f44d40a93994bf7af4e4d62fb1c98b729b31
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-05 05:34:13 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,36 @@
+// primitive 
+
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scorevalue = 100.3
+const isLoggedIn =false
+const outsidetemp = null
+let userEmail; //undefined value 
+ 
+const id = Symbol('123')
+const anotherId = Symbol('123') 
+console.log(id === anotherId);
+  // symbols is a unique declaration of any object 
+const Bignumber = 466675577856n
+
+
+
+// array,objects,functions
+ 
+
+const heros = ["Superman","Ironman","spiderman"] //arrays
+ let myobj = {
+    name: "jagrati",
+    age : "20",
+ }
+  const myfunction = function(){
+    console.log("Hello world");
+    }
+
+    console.log(typeof Bignumber);
+    console.log(typeof isLoggedIn);
+    console.log(typeof outsidetemp);
+    console.log(typeof myfunction);
+    console.log(typeof id);
