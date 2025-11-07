4d1424ad9b6be79169675929839a5b54632587c9
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-07 07:03:40 +0000

    strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,12 @@
+const name = "jagrati"
+const repocount = 50 
+//  console.log(name + repocount + "value");
+//  don't use this syntax this syntax is outdatted 
+
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+
+//The above syntax is much readable and easy to use //
+
+
+const gameName = new String('jagrati')
+
