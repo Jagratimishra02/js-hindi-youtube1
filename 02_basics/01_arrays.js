102680f39f506ae420d0234d4660b1a0ce06cee5
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-09 08:39:47 +0000

    01_arrays.js

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+// arrays
+
+// const myArray = [1,2,3,4,5]
+// const myArray01 = [1,"2",a,true] 
+// // elements inside array can be of same datatype or can be of different type also.
+  
+let myArray1 = [name= "jagrati",
+    email = "j@email.com"
+]
+let myArray2 = myArray1
+  myArray2.email = "n@email.com"
+console.log(myArray1.email);
+console.log(myArray2.email);
+// here email present in my array1 changes because myarray2=myarray1 means now they have same reference if we changes myarray2.email myarray1 email also changes
+
+
+// ++++++++++ similarly +++++++++//
+// let myArray1 = [name= "jagrati",
+//     email = "j@email.com"
+// ]
+// let myArray2 = myArray1
+//   myArray2.name = "nishi"
+// console.log(myArray1.name);
+// console.log(myArray2.name);   // doubt
+
+
