32393a1fae0312ac337ae9c5d8c3e58b49b71f1d
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-17 03:27:54 +0000

    02_arrays.js

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,5 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+const dc_heros = ["superman","flash","batman"]
+
+marvel_heros.push(dc_heros) // array dc_heros pushed directly to marvel_heros ,means array inside array
+console.log(marvel_heros);
