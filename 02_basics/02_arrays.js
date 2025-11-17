91ec1e00886fd115ae32b85da984a602c0f711d1
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-17 14:09:27 +0000

    02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -12,5 +12,17 @@ const dc_heros = ["superman","flash","batman"]
 //const allHeros = marvel_heros.concat(dc_heros)
 //console.log(allHeros);
 
+const all_new_heros = [...marvel_heros, ...dc_heros]
+console.log(all_new_heros); // it concates every element of arrays and convert it into strings
+const arrayA = [1, 2, 3,[4, 5, 6], 7, [6,7,[4, 5]]]
+const real_arrayA = arrayA.flat(Infinity)
+console.log(real_arrayA); // it remove the paranthesis it removes the arrays inside array
+//values r spread out
+console.log(Array.isArray("jagrati")); // output is false
 
-
+console.log(Array.from("jagrati"));
+console.log(Array.from({name: "jagrati"}));//this array gives empty array 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
