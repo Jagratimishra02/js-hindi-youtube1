ef06efe10067d75f8f38451fa637f03305fdcd39
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-17 06:31:56 +0000

    02_arrays.js

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,5 +1,16 @@
 const marvel_heros = ["thor", "ironman", "spiderman"]
 const dc_heros = ["superman","flash","batman"]
 
-marvel_heros.push(dc_heros) // array dc_heros pushed directly to marvel_heros ,means array inside array
-console.log(marvel_heros);
+// marvel_heros.push(dc_heros) // array dc_heros pushed directly to marvel_heros ,means array inside array
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]); // means 1st Element of 3rd Element which is array
+
+
+// marvel_heros.concat(dc_heros)
+// console.log(marvel_heros); // concat makes new array while push works on existing array
+
+//const allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros);
+
+
+
