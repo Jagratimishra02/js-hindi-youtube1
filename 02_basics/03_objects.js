374fb73c8bba1e21210336baf7af8974c59d97f2
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-21 15:11:31 +0000

    03_objects.js

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,36 +2,35 @@
 
 // object literals
 //Object.create
-const [mySym ]= symbol("key1")
+const mySym = Symbol("key1")
 const jsUser = {
     name: "jagrati",
     "full name": "jagrati mishra",
-  //  mysym: "mykey1",
+  [mySym] : "mykey1",
     age: 20,
     location: "bhopal",
     email: "jagrati@google.com",
     isloggedIn: false,
     lastLoginDays:["monday","saturday"],
-     [mySym]: "key" ,
+      
 }
 console.log(jsUser.email);
-console.log(jsUser["email"]);// here we have acces email in strings
-
-
+console.log(jsUser["email"]);// here we have acces email in strings 
 
 console.log;(jsUser["full name"]); // strings like full name should acces like this
-
-
-
 
 //console.log(jsUser.mysym);
 //.log(typeof jsUser.mysym); // string but we has given symbol so use brackets
 
 
 console.log(jsUser[mySym]);
-console.log(typeof jsUser[mySym]);
+console.log(typeof jsUser[mySym]); // this gives the value inside mysym which is "mykey1" which is string type
+console.log(typeof mySym);// type is symbol
 
-
+jsUser.email = "jago@google.com" // object is freeze 
+Object.freeze(jsUser)
+ jsUser.email = "jagrati@microsoft.com"  // email will not change bcoz it is freezed
+console.log(jsUser);
 
 
 
