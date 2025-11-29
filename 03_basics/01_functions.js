8a7944b7d4a26f94126fc85547d05b3da27aa6ff
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-29 05:09:38 +0000

    functions

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -8,7 +8,7 @@
 function sayMyname(){
     console.log("n");
     console.log("i");
-    console.log("s");                      // this fmethod is readable
+    console.log("s");                      // this method is readable
     console.log("h");
     console.log("i");
 }
@@ -20,10 +20,10 @@ sayMyname()
   console.log( number1 + number2);
   }
   addTwoNumbers()   // Nan bcoz there r no arguments means value is not given in n0.1 and n0.2
-  function addTwoNumbers(number1,number2){
+  function additionTwoNumbers(number1,number2){
   console.log( number1 + number2);
   }
-  addTwoNumbers(2,4) 
+  additionTwoNumbers(2,4) 
   
 
 ////////////////////
@@ -89,3 +89,43 @@ function loginusermessage(username) {
  }
  console.log(loginpopup());
   console.log(loginpopup("jago")); //automatic jago will be overwrite automatically
+
+
+//+++++++++++++++++++++++++++++++++++++++++
+
+
+function calculateCartPrice(...num1) {
+  return num1
+}
+console.log(calculateCartPrice(200,500,600));  
+ //  all the prices r added bcoz of ...num1 "... 3 dot this rest operator"
+
+
+function calculatenewCartPrice(value1,value2,value3,...num1) {
+  return num1
+}
+console.log(calculatenewCartPrice(200,500,600,900,3000)); // 200,500,600 is stored in value1,value2,value3 rest is stored in array of num 1
+
+const user = {
+  username: "nishi",
+  price: 199
+}
+function handleobject(anyobject) {
+  console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+handleobject(user)
+  
+//another method to call object
+
+handleobject({
+  username : "jago",
+  price : 400
+})
+
+const myNewArray = [200,300,400,500]
+function returnsecondvalue(getarray) {
+  return getarray[2]
+}
+console.log(returnsecondvalue(myNewArray));
+console.log(returnsecondvalue([200,300,400,500]
+));
