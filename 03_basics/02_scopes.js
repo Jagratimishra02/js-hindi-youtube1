554d12e416cf99670b26f6227e100f8cd6ec0234
Jagratimishra02 <nishimisra002@gmail.com>
2025-11-30 17:26:54 +0000

    scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -9,6 +9,7 @@ if (true) {
 
 // console.log(a);
 // console.log(b); // these elements  should not be available outside the block scope , value inside the scope should not be access outside the block scope
+
 console.log(c);
 // c will be 30 instead of 300 becoz of var 300 so don't use var
 
@@ -17,9 +18,23 @@ let boya = 100
 const boyb = 200
 var boyc = 300
   
-if (true) { 
+if (!false) { 
     let boya = 10 
     
 }
 console.log(boya); // it will access value 100 not 10 bcoz value should not accessed outside the block scope
 
+var smartphones = 20000
+const price = 50000
+if (true) {
+let smartphones = 100
+const price = 40000
+console.log("Inner:",smartphones);
+}
+console.log(smartphones);
+// value inside inner is 100 while outside 20000
+
+
+// for (let i = 0; i < Array.length; i++) {
+//     const elements = Array[i];
+// }
