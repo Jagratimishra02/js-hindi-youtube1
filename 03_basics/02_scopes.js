0dde4d3146f29ad91739117cff46490fcca47eae
Jagratimishra02 <nishimisra002@gmail.com>
2025-12-01 15:16:37 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -38,3 +38,47 @@ console.log(smartphones);
 // for (let i = 0; i < Array.length; i++) {
 //     const elements = Array[i];
 // }
+
+
+function one() {
+    const username = "jagrati"
+
+    function two() {
+        const website = "youtube"
+        console.log(username);  
+        
+    }
+    // console.log(website);//this line will not execute outside
+
+    two()
+    
+}
+one()
+
+
+if (true) {
+    const username = "jagrati"
+    if (username === "jagrati") {
+        const website = "youtube"
+        console.log(username + website); 
+    }
+    // console.log(website);  // value outside the scope can't be accessed
+}
+// console.log(username);
+
+
+//++++++++++++++ imp  ++++++++++
+
+
+console.log(addOne(5))
+
+function addOne(num) {
+    return num + 1
+}
+//  addTwo(5)        // this can't be accessed , don't use the function before declaration
+
+ const addTwo = function(num){
+    return num + 2
+ }
+addTwo(5)
+console.log(addTwo); // output => [Function: addTwo]
