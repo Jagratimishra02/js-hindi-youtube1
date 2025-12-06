799dfb034e4ad1f7831256c9339442b845a170d0
Jagratimishra02 <nishimisra002@gmail.com>
2025-12-06 04:08:43 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,5 +1,18 @@
 // Immediately Invoked function expression (iife)
 
-function maggie() {
-    console.log(`DB connected `);
-}
+// function maggie() {
+//     console.log(`DB connected`);
+// }
+// maggie()
+       
+
+//++++++++++++++++++++++++++++++++++//
+//global scopes created problem so to overcome this problem we use iife 
+
+(function coffee() {
+    console.log(`DB connected`);
+})()
+
+()()
+
+
