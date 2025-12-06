0aed8170b89b59e8e7195f23a37703cea741e143
Jagratimishra02 <nishimisra002@gmail.com>
2025-12-06 09:51:31 +0000

    iife

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,18 +1,28 @@
 // Immediately Invoked function expression (iife)
 
-// function maggie() {
-//     console.log(`DB connected`);
-// }
-// maggie()
+function maggie() {
+    console.log(`DB connected`);
+}
+maggie();
        
 
 //++++++++++++++++++++++++++++++++++//
-//global scopes created problem so to overcome this problem we use iife 
+// global scopes created problem so to overcome this problem we use iife 
 
-(function coffee() {
+(function coffee() {                   // named iffe which has name coffee
     console.log(`DB connected`);
-})()
+})();
 
-()()
+//semicolon is imp otherwise code will behave as a continuation nd throw error
+
+( () => {
+    console.log(`db connected 2`);
+})() ;  // arrow function can also  be executed by iife
 
 
+// +++++++++++++ iife syntax ()()  if u r using multiple iifi use semicolon to end 
+
+
+( (name) => {                            // unnamed iife
+  console.log(`db connected 3 ${name}`);                         
+})('nishu');
