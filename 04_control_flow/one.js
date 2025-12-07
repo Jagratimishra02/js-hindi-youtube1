0572a5a75f649e53f972d38c9b1e6e79e30141d7
Jagratimishra02 <nishimisra002@gmail.com>
2025-12-07 07:26:22 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,51 @@
+//if
+
+// if (true){
+
+// }
+
+const isUserloggedIn = true
+const temperature = 21
+if (2 == "2") {                 
+    console.log("executed");  // it will execute
+}
+if (2 === "2") {              // === is a strict it  also check the type of data type 
+    console.log("executed");  // it will not execute
+}
+if ( temperature < 50 ) {                 
+    console.log("less than 50");
+}
+     else {
+         console.log("temperature is greater than 50");
+    }           // if we use both if/else one statement can be executed only
+    console.log("executed"); // it will execute in every condition 
+    
+// operations :- <,>,<=,>=,!=, === , !==
+
+const score = 200 
+if (score > 100) {
+    const power = "fly"
+    console.log(`user power: ${power}`);
+}
+// console.log(`user power: ${power}`);  // scope cannot the values outside 
+
+
+//  //++++++++++++++or++++++++++
+// const balance 
+//  if (balance > 50) console.log("test"),
+//  console.log("test2"); // unreadable code 
+const balance = 1000
+  if (balance < 500) {
+    console.log("less than 500");
+  } 
+      else if (balance < 750) {
+    console.log("less than 750");
+  } 
+      else if (balance < 1000) {
+        console.log("less than 1000");
+      }
+ else {
+    console.log(" balance is greater than or equal to 1000");      // result will be less than or equal to 1200
+ }
+
+ 
