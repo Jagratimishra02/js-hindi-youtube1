b91ab77429bf31318cba13c20298e07b3428af27
Jagratimishra02 <nishimisra002@gmail.com>
2025-12-22 05:26:36 +0000

    truthy.js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,23 @@
+const userEmail = "j@jago.ai"
+if (userEmail) {
+console.log("got userEmail");
+} else {
+    console.log("dont have userEmail");
+}
+
+const usergmail = ""  // for empty strings value is false so else is executed 
+if (usergmail) {
+console.log("got usergmail");
+} else {
+    console.log("dont have gmail");
+}
+
+
+const usermail = [] // for array value is true so if executed nd result is got usermail
+if (usermail) {
+console.log("got usermail");
+} else {
+    console.log("dont have usermail");
+}
+
+//truthy values
