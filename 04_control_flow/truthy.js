7511201d62347cc3bc4962445e17711a60b1dce6
Jagratimishra02 <nishimisra002@gmail.com>
2025-12-23 06:23:20 +0000

    truthy.js

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -20,4 +20,36 @@ console.log("got usermail");
     console.log("dont have usermail");
 }
 
-//truthy values
+//falsy values :- 0,-0,"",Bigint,null,undefined,NaN
+
+
+// truthy values :- "0"," ","undefined",{},[],function (){}
+ 
+
+if (usermail.length===0) {
+    console.log("array is empty");
+}
+
+const emptyObj = {}
+if (Object.keys(emptyObj).length === 0) {  
+console.log("obj is empty");
+}
+
+// nullish coalescing operater (??) : NULL UNDEFINED
+
+let val1;
+// val1 = 5 ?? 10                      // output = 5
+// val1 = null ?? 10                   //output = 10
+// val1 = undefined ?? 15              //output = 15
+val1 = null ?? 10 ?? 12                //output = 10
+
+console.log(val1);
+
+// terniary operator
+// conition ? true : false 
+
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");
+
+
