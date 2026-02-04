f9e32cda8933f58f00e2bbf38f33454a19f7126c
Jagratimishra02 <nishimisra002@gmail.com>
2026-02-04 06:02:14 +0000

    chnages

--- 05_iterations/06_six.js
+++ 05_iterations/06_six.js
@@ -30,3 +30,30 @@ const myNums = [1,2,3,4,5,6,7,8,9,10]
 
 
 const newNums = []
+
+// myNums.forEach((Num) => {
+//     if (Num>4){
+//         newNums.push(Num)
+//     }
+// });
+// console.log(newNums);
+
+
+const books = [
+    { title : 'book one'   , genre : 'fiction'     , publish : 1981 , edison : 2004 },
+    { title : 'book two'   , genre : 'non-fiction' , publish : 1992 , edison : 2008 },
+    { title : 'book three' , genre :  'History'    , publish : 1999 , edison : 2007 },
+    { title : 'book four'  , genre :'non-fiction'  , publish : 1989 , edison : 2010 },
+    { title : 'book five'  , genre : 'science'     , publish : 2009 , edison : 2014 },
+    { title : 'book six'   , genre : 'fiction'     , publish : 1987 , edison : 2010 },
+    { title : 'book seven' , genre : 'History'     , publish : 1986 , edison : 1996 },
+    { title : 'book eight' , genre : 'science'     , publish : 2011 , edison : 2016 },
+    { title : 'book nine'  , genre : 'fiction'     , publish : 1981 , edison : 1989 } 
+];
+ 
+
+let userBooks = books.filter( (books) =>books.genre === 'History' )
+console.log(userBooks);
+
+ userBooks = books.filter((books) =>books.publish >= 1995)
+console.log(userBooks);
