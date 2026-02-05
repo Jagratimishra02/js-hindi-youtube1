d00aa7c8e6cdc06a0d65ac18622181d04f9a55a5
Jagratimishra02 <nishimisra002@gmail.com>
2026-02-05 14:33:07 +0000

    changes

---
+++ 05_iterations/08_eight.js
@@ -0,0 +1,5 @@
+const myNums = [1,2,3]
+const initialvalue = 0;
+const sumwithinitial = myNums.reduce((accumulator, currentvalue) => accumulator + currentvalue,initialvalue);
+console.log(sumwithinitial);
+
